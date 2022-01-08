2495344d4745b8201ba9767cda1df57933cc8ec9
TWCoding <96042704+TWCoding@users.noreply.github.com>
2022-01-08 22:56:36 +0800

    Update d060.cpp

--- d060.cpp
+++ d060.cpp
@@ -1,11 +1,10 @@
 #include <iostream>
-#include <cmath>
 
 using namespace std;
 
 int main(){
 	int n;
 	cin >> n;
-	cout << (abs(60-n)+25)%60 << "\n";
+	cout << (60-n+25)%60 << "\n"; 
 	return 0;
 }
