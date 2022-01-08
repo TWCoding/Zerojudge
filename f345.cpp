6fd1195b21ef370f2ea776a4804073d4fb1f7a6f
TWCoding <96042704+TWCoding@users.noreply.github.com>
2022-01-08 12:02:33 +0800

    Update f345.cpp

--- f345.cpp
+++ f345.cpp
@@ -4,11 +4,11 @@ using namespace std;
 
 int main(){
 	int n;
-	while(cin>>n){
-		int a[n]={0};
-		for(int i=0;i<n;i++){
-			cin >> a[i]; 
-		}
+	cin>>n
+	int a[n]={0};
+	for(int i=0;i<n;i++){
+		cin >> a[i]; 
+	}
 	for(int i=n-1;i>=0;i--){
 		cout <<	a[i] << " "; 
 	}
