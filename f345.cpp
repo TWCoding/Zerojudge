79e356d90b88085e756d94c34b32079e5e13657c
TWCoding <96042704+TWCoding@users.noreply.github.com>
2022-01-08 11:59:53 +0800

    Add files via upload

---
+++ f345.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+	int n;
+	while(cin>>n){
+		int a[n]={0};
+		for(int i=0;i<n;i++){
+			cin >> a[i]; 
+		}
+	for(int i=n-1;i>=0;i--){
+		cout <<	a[i] << " "; 
+	}
+	cout << endl;
+	}
+return 0; 
+}
