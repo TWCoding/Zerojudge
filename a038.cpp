111413560207529381522a8077f41df02c4bd4b2
TWCoding <96042704+TWCoding@users.noreply.github.com>
2022-04-23 10:28:42 +0800

    Add files via upload

---
+++ a038.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+	int n;
+	cin>>n;
+	if(n==0){
+		cout << "0" << endl;
+	}
+	while(n%10==0&&n!=0){
+		n/=10;
+	}
+	while(n>0){
+		cout << n%10 ;
+		n/=10;
+	}	
+	cout << endl;
+	return 0;
+} 
